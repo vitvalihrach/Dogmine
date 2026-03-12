69f59441dfe2319cfe674d96c660346dca338c3a
vitvalihrach <vitvalihrach@seznam.cz>
2026-03-12 18:43:57 +0100

    Refactor DogmineApp initialization and routes

--- mobile/dogmine_app/lib/main.dart
+++ mobile/dogmine_app/lib/main.dart
@@ -6,23 +6,21 @@ import 'package:dogmine_app/presentation/screens/home_screen.dart';
 import 'package:dogmine_app/presentation/screens/login_screen.dart';
 
 void main() {
-  runApp(const DogmineApp());
+  runApp(DogmineApp());
 }
 
 class DogmineApp extends StatelessWidget {
-  const DogmineApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dogmine',
       initialRoute: '/home',
-      routes: <String, WidgetBuilder>{
-        '/home': (context) => const HomeScreen(),
-        '/history': (context) => const HistoryScreen(),
-        '/camera': (context) => const CameraScreen(),
-        '/login': (context) => const LoginScreen(),
+      routes: {
+        '/home': (context) => HomeScreen(),
+        '/history': (context) => HistoryScreen(),
+        '/camera': (context) => CameraScreen(),
+        '/login': (context) => LoginScreen(),
       },
     );
   }
