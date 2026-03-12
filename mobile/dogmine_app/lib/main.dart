a019449dc8f2449a0c61080f3a5aa086e513ac77
vitvalihrach <vitvalihrach@seznam.cz>
2026-03-12 13:33:20 +0100

    Set up initial routes for navigationAdd routes for home/history/camera
    Added routes for home, history, camera, and login screens.

--- mobile/dogmine_app/lib/main.dart
+++ mobile/dogmine_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:dogmine_app/presentation/screens/home_screen.dart';
+import 'package:dogmine_app/presentation/screens/history_screen.dart';
+import 'package:dogmine_app/presentation/screens/camera_screen.dart';
+import 'package:dogmine_app/presentation/screens/login_screen.dart';
 void main() {
   runApp(DogmineApp());
 }
@@ -8,6 +11,15 @@ class DogmineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+  debugShowCheckedModeBanner: false,
+  initialRoute: '/home',
+  routes: {
+    '/home': (context) => HomeScreen(),
+    '/history': (context) => HistoryScreen(),
+    '/camera': (context) => CameraScreen(),
+    '/login': (context) => LoginScreen(),
+  },
+);
       title: 'Dogmine',
       theme: ThemeData(
         primarySwatch: Colors.blue,
